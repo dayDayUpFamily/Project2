82a003452beeed1f3af090d764ce8cb262941a52
Krist <krist333@126.com>
2015-04-01 00:57:50 -0400

    tweaks

--- authenticate.js
+++
@@ -1,7 +0,0 @@
-var User = require('./models/User');
-var jwt = require('jwt-simple');
- 
-module.exports = function(req, res, next) {
-	var token = (req.body && req.body.access_token) || (req.query && req.query.access_token) || req.headers['x-access-token'];
-	
-};
