6483627571c1f047dd4729985e126b1accfcdd9e
gushanlongchuan <53longchuan@sina.com>
2015-04-08 23:16:19 -0400

    nonce complete

--- middleware/nonce.js
+++ middleware/nonce.js
@@ -3,8 +3,18 @@ var redis_client = redis.createClient();
 
 module.exports = {
     check_nonce:function(req, res,next) {
-        console.log(req.headers['nonce']);
-        next();
+        var req_nonce = req.headers['nonce'];
+        redis_client.get(req_nonce, function (err, reply) {
+            if(!reply) {
+                redis_client.set(req_nonce, 1);
+                console.log('get new nonce');
+                next();
+            }
+            else {
+                res.sendStatus(404);
+                console.log("dupilcate request");
+            }
+        })
     }
 
 }
