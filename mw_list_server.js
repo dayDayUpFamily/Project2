0a57a7ce12dd6f43b351fd5fb8067dacf25b7abb
j3y2z1 <j3y2z1@126.com>
2015-04-20 14:49:57 -0400

    mw-list_server async

--- mw_list_server.js
+++ mw_list_server.js
@@ -5,6 +5,7 @@ var MW_after = require('./models/MW_after');
 var beforeList = [];
 var afterList = [];
 var mongoose = require("mongoose");
+var async = require('async');
 
 mongoose.connect('mongodb://krist:krist@ds059471.mongolab.com:59471/express-test');
 
@@ -17,30 +18,38 @@ app.get('/mwlist/:biz_name', function (req, res) {
   beforeList = [];
   afterList = [];
   // console.log(req.params.biz_name);
+  async.parallel([beforefind,afterfind],function(err,result){
+    console.log(result);
+    if(err)return res.status(403).send('query middleware configuaration error');
+     return res.send("beforeList:"+beforeList+"\n"+"afterList:"+afterList+"\n");
+
+  })
+  function beforefind(callback){
   MW_before.find({service_name: req.params.biz_name}, function(err, middlewares) {
      console.log("before list",middlewares);
-    if (err)
-      return res.status(403).send('query middleware configuaration error');
+    if (err) return callback(err);
     else
     {
       // console.log(19);
       for(i = 0; i < middlewares.length; i++)
         beforeList.push(middlewares[i].mw_name);
-      // return res.send(beforeList);
+      return callback(null,beforeList);
     }
   });
-  // console.log(25);
+  }
+  function afterfind(callback){
   MW_after.find({service_name: req.params.biz_name}, function(err, middlewares) {
     console.log("after list",middlewares);
     if (err)
-      return res.status(403).send('query middleware configuaration error');
+      return callback(err);
     else
     {
       for(i = 0; i < middlewares.length; i++)
         afterList.push(middlewares[i].mw_name);
-      // return res.send("beforeList:"+beforeList+"\n"+"afterList:"+afterList+"\n");
+      return callback(null,afterList);
     }
   });
+  }
   // res.send(beforeList);
   // console.log(37);
 });
@@ -49,24 +58,32 @@ app.put('/mwlist/:biz_name/:mw_name/:new_priority', function (req, res) {
   var service_name=req.params.biz_name;
   var mw_name=req.params.mw_name;
   var new_priority=req.params.new_priority;
-  MW_before.update({service_name: service_name,mw_name:mw_name},{$set:{"priority":new_priority}}, function(err, middlewares) {
-    console.log("before list",middlewares);
-    if (err)
-      return res.status(403).send('query middleware configuaration error');
-    else
-    {
-      //return res.status(200).send("update success");
-    }
-  });
+
+  async.parallel([beforeupdate,afterupdate ],function(err,result){
+    console.log(result);
+    if(err)return res.status(403).send('update middleware configuaration error');
+    return res.status(200).send("update success");
+
+  })
+   function beforeupdate(callback) {
+     MW_before.update({
+       service_name: service_name,
+       mw_name: mw_name
+     }, {$set: {"priority": new_priority}}, function (err, middlewares) {
+       console.log("before list", middlewares);
+       if (err)
+         return callback(err);
+       return callback(null);
+     });
+   }
+  function afterupdate(callback){
   MW_after.update({service_name: service_name,mw_name:mw_name},{$set:{"priority":new_priority}}, function(err, middlewares) {
     console.log("after list",middlewares);
     if (err)
-      return res.status(403).send('query middleware configuaration error');
-    else
-    {
-      return res.status(200).send("update success");
-    }
+      return callback(err);
+     return callback(null);
   });
+  }
 
 });
 
